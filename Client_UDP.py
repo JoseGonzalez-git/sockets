f0a8fd9da6f1930eb598e600c7252c9c7fbfa84e
Jose Alejandro Gonzalez Ortiz <70743629+JoseGonzalez-git@users.noreply.github.com>
2022-07-21 19:13:45 -0500

    v2

--- Client_UDP.py
+++ Client_UDP.py
@@ -1,36 +1,55 @@
 import socket
-# Nombre del servidor
-serverName = "localhost"
-# ServerPort corresponde al puerto por donde escucha el servidor
-serverPort = 12000
-# Se instancia un cliente socket de tipo UDP
-client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-# Extrae del teclado una palabra escrita por el cliente
-initial_sms = print("A continuacion complete los sgtes datos: ")
-validator_client = True
-while validator_client:
-    id = int(input("Ingrese el ID del cliente: "))
-    name = input("Ingrese el nombre del cliente: ")
-    salary = float(input("Ingrese el salario del cliente: "))
-    # Se crea una tupla con los datos del cliente
-    client_data:str = '{}, {}, {}'.format(id, name, salary)
-    # Se envia la tupla al servidor
-    client_socket.sendto(str(client_data).encode(), (serverName, serverPort))
-    # Se recibe la respuesta del servidor
-    message, serverAddress = client_socket.recvfrom(2048)
-    # Se imprime la respuesta del servidor
-    print("El servidor respondio: " + message.decode())
-    # Se pregunta al cliente si desea ingresar otro cliente
-    clientContinue = input("Desea ingresar otro cliente? (s/n): ")
-    if clientContinue.upper() == "N":
-        validator_client = False
-    elif clientContinue.upper() != "S" and clientContinue.upper() != "N":
+from tkinter import E
+
+def main():
+    # Nombre del servidor
+    server_name = "localhost"
+    # server_port corresponde al puerto por donde escucha el servidor
+    server_port = 12000
+    # Se instancia un cliente socket de tipo UDP
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    # Extrae del teclado una palabra escrita por el cliente
+    print("A continuacion complete los sgtes datos: ")
+    # Se ejecuta el comunicador
+    comunication_to_client(client_socket, server_name, server_port)
+
+def comunication_to_client(client_socket, server_name, server_port):
+    while True:
+        identification = int(input("Ingrese el ID del cliente: "))
+        name = input("Ingrese el nombre del cliente: ")
+        salary = float(input("Ingrese el salario del cliente: "))
+        # Se crea una tupla con los datos del cliente
+        client_data: str = '{},{},{}'.format(identification, name, salary)
+        # Se envia la tupla al servidor
+        client_socket.sendto(str(client_data).encode(),
+                            (server_name, server_port))
+        # Se recibe la respuesta del servidor
+        message, server_address = client_socket.recvfrom(2048)
+        # Se imprime la respuesta del servidor
+        print("El servidor respondio: " + message.decode())
+        print("\nDesde: ", server_address)
+        # Se pregunta al cliente si desea ingresar otro cliente
+        validator_client = client_contunue(client_socket, server_name, server_port)
+        if validator_client == False:
+            # Se recibe la respuesta del servidor
+            message, server_address = client_socket.recvfrom(2048)
+            print("Informacion almacenada en el servidor: ")
+            print(message.decode())
+            # Se cierra el socket
+            client_socket.close()
+            print("El cliente se ha cerrado")
+            exit()
+
+def client_contunue(client_socket, server_name, server_port):
+    client_continue = input("Desea ingresar otro cliente? (s/n): ")
+    if client_continue.upper() == "N":
+        client_socket.sendto('Fin'.encode(), (server_name, server_port))
+        return False
+    elif client_continue.upper() != "S" and client_continue.upper() != "N":
         print("Ingrese una opcion valida")
-        clientContinue = input("Desea ingresar otro cliente? (s/n): ")
+        client_contunue(client_socket, server_name, server_port)
     else:
-        validator_client = True
-# Se cierra el socket
-client_socket.close()
-print("El cliente se ha cerrado")
-# Se termina el programa
-exit()
+        return True
+
+
+main()
--- Server_UDP.py
+++ Server_UDP.py
@@ -1,16 +1,41 @@
-from socket import *
-serverPort = 12000
-serverSocket = socket(AF_INET, SOCK_DGRAM)
-serverSocket.bind(("", serverPort))
-print("El servidor esta listo para recibir datos")
-message = "El servidor recibio el dato correctamente."
-db =[]
-while 1:
-    data, clientAddress = serverSocket.recvfrom(2048)
-    print("El cliente envio: ")
-    for i in data.decode():
-        print(i, end="")
-    print("\nDesde: ", clientAddress)
-    db.append(data.decode().split(","))
-    print("\nLa base de datos actual es: ", db)
-    serverSocket.sendto(message.encode(), clientAddress)
+import socket
+
+def main():
+    server_port = 12000
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    server_socket.bind(("", server_port))
+    print("El servidor esta listo para recibir datos")
+    comunication(server_socket)
+
+def comunication(server_socket):
+    db =[]
+    while 1:
+        data, client_address = server_socket.recvfrom(2048)
+        print("El cliente envio: ")
+        if not data.decode().__contains__("Fin"):
+            for i in data.decode():
+                print(i, end="")
+            append_db(data.decode().split(","),db)
+            print("\nDesde: ", client_address)
+            print("\nLa base de datos actual es: ", db)
+            message = "El servidor recibio el dato correctamente."
+            server_socket.sendto(message.encode(), client_address)
+        else:
+            print(data.decode())
+            order_db(db)
+            message = ""
+            for i in db:
+                message += str(i[0]) + "," + i[1] + "," + str(i[2]) + "\n"
+            print(message)
+            server_socket.sendto(message.encode(), client_address)
+            server_socket.close()
+            print("El servidor se ha cerrado")
+            break
+
+def order_db(db):
+    return db.sort(key=lambda x: x[0])
+
+def append_db(data,db):
+    db.append([int(data[0]), data[1], float(data[2])])
+
+main()
